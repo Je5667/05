7dbc47420fe6a640161681b7ff6cb838b2d0090b
Je5667 <113067745+Je5667@users.noreply.github.com>
2022-10-04 16:40:04 +0900

    02 수정본

--- main.c
+++ main.c
@@ -9,8 +9,8 @@ int main(int argc, char *argv[]) {
 	printf("정수 하나를 입력하시오: ");
 	scanf("%d",&x);
 	
-	if (x>=0)
-		printf("절대값은  %d입니다", x);
-	else
-		printf("절대값은 %d입니다",-x);
+	if (x<0)
+		x=-1*x;
+		
+	printf("절대값은 %d입니다",x);
 }
